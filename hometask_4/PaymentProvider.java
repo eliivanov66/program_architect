aa552d27669ec099404d7f0cf8f94d46509de864
eliivanov66 <109104693+eliivanov66@users.noreply.github.com>
2023-09-08 10:47:35 +0500

    commit after hometask 4

---
+++ hometask_4/PaymentProvider.java
@@ -0,0 +1,8 @@
+public class PaymentProvider{
+    
+    public boolean buyTicket(int orderId, String cardNo, double amount){
+        //TODO: Обращение к платежному шлюзу, попытка выполнить списание средств ...
+        return true;
+    }
+
+}
